acb238a2effe0b4b23d06598bce6a7b5c825fe32
김상현 <llmm030506@naver.com>
2021-01-13 23:25:24 +0900

    [ADD]
    - viewController 추가 (페이지 라우팅)

---
+++ src/main/java/com/helpme/MembershipFee/domains/controller/ViewController.java
@@ -0,0 +1,51 @@
+package com.helpme.MembershipFee.domains.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1")
+@Controller
+public class ViewController {
+
+    @GetMapping("/main")
+    public String mainPage(){
+        return "main";
+    }
+
+    @GetMapping("/deposit")
+    public String depositPage(){
+        return "deposit";
+    }
+
+    @GetMapping("/depositview")
+    public String depositViewPage(){
+        return "depositView";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping("/memberadd")
+    public String memberAdd(){
+        return "memberAdd";
+    }
+
+    @GetMapping("/membershipfeeadd")
+    public String membershipFeeAdd(){
+        return "membershipFeeAdd";
+    }
+
+    @GetMapping("/membershipfeeview")
+    public String membershipFeeView(){
+        return "membershipFeeView";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+}
