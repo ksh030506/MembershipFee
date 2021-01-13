4df36cc1924a9dd210956e270afc09ec08b7a521
김상현 <llmm030506@naver.com>
2021-01-13 16:44:23 +0900

    Initial commit

---
+++ src/main/java/com/helpme/MembershipFee/MembershipFeeApplication.java
@@ -0,0 +1,13 @@
+package com.helpme.MembershipFee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipFeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipFeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpme/MembershipFee/MembershipFeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpme.MembershipFee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipFeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
