0603307eec249d64983b5521bca0ed8b301a5b49
김상현 <llmm030506@naver.com>
2021-01-14 20:20:49 +0900

    [ADD]
    - 입급 권한

--- src/main/java/com/helpme/MembershipFee/service/DepositService.java
+++ src/main/java/com/helpme/MembershipFee/service/DepositService.java
@@ -1,9 +1,14 @@
 package com.helpme.MembershipFee.service;
 
+import com.helpme.MembershipFee.common.JwtUtil;
+import com.helpme.MembershipFee.domain.administratorMember.AdministratorMember;
+import com.helpme.MembershipFee.domain.administratorMember.AdministratorMemberRepository;
 import com.helpme.MembershipFee.domain.deposit.DepositRepository;
 import com.helpme.MembershipFee.web.dto.DepositSaveRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Service
 public class DepositService {
@@ -11,8 +16,19 @@ public class DepositService {
     @Autowired
     private DepositRepository depositRepository;
 
-    public Long save(DepositSaveRequestDto depositSaveRequestDto){
+    @Autowired
+    private AdministratorMemberRepository administratorMemberRepository;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public Long save(DepositSaveRequestDto depositSaveRequestDto, HttpServletRequest req){
+        final String token = req.getHeader("userEmail");
+        String userEmail = jwtUtil.getUserEmail(token);
+        AdministratorMember administratorMember = administratorMemberRepository.findByEmail(userEmail);
+        if(jwtUtil.validateToken(token, administratorMember) == false){
+            throw new IllegalArgumentException("로그인을 해주세요");
+        }
         return depositRepository.save(depositSaveRequestDto.toEntity()).getIdx_Deposit();
     }
-
 }
--- src/main/java/com/helpme/MembershipFee/web/controller/DepositController.java
+++ src/main/java/com/helpme/MembershipFee/web/controller/DepositController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RequestMapping("/api/v1")
 @RestController
 public class DepositController {
@@ -16,8 +18,8 @@ public class DepositController {
     private DepositService depositService;
 
     @PostMapping("/adddeposit")
-    public String AddDeposit(@RequestBody DepositSaveRequestDto depositSaveRequestDto){
-        depositService.save(depositSaveRequestDto);
+    public String AddDeposit(@RequestBody DepositSaveRequestDto depositSaveRequestDto, HttpServletRequest req){
+        depositService.save(depositSaveRequestDto, req);
         return "저장 완료";
     }
 
