e1033508e2f00ed59b6d92540cfd8faab63310be
김상현 <llmm030506@naver.com>
2021-01-15 20:28:27 +0900

    [UPDATE]
    - 입금 수정

--- src/main/java/com/helpme/MembershipFee/service/MemberShipFeeService.java
+++ src/main/java/com/helpme/MembershipFee/service/MemberShipFeeService.java
@@ -3,6 +3,7 @@ package com.helpme.MembershipFee.service;
 import com.helpme.MembershipFee.common.JwtUtil;
 import com.helpme.MembershipFee.domain.administratorMember.AdministratorMember;
 import com.helpme.MembershipFee.domain.administratorMember.AdministratorMemberRepository;
+import com.helpme.MembershipFee.domain.deposit.DepositRepository;
 import com.helpme.MembershipFee.domain.member.Member;
 import com.helpme.MembershipFee.domain.membershipfee.MemberShipFee;
 import com.helpme.MembershipFee.domain.membershipfee.MemberShipFeeRepository;
@@ -23,10 +24,13 @@ public class MemberShipFeeService {
     private AdministratorMemberRepository administratorMemberRepository;
 
     @Autowired
+    private DepositRepository depositRepository;
+
+    @Autowired
     private JwtUtil jwtUtil;
 
     @Transactional
-    public Long save(MemberShipFeeSaveRequestDto memberShipFeeSaveRequestDto, HttpServletRequest req){
+    public Long save(MemberShipFeeSaveRequestDto memberShipFeeSaveRequestDto, HttpServletRequest req) throws Exception {
         final String token = jwtUtil.GetTokenByHeader(req);
         String userEmail = jwtUtil.getUserEmail(token);
         //AdminIdx 저장을 위한 관리자 불러오기
@@ -38,7 +42,15 @@ public class MemberShipFeeService {
                 .administratorMember(administratorMember)
                 .build();
         jwtUtil.validateToken(token, administratorMember);
+        BalanceCheck(memberShipFeeSaveRequestDto.getMembershipfee());
         return memberShipFeeRepository.save(memberShipFee).getIdx_Membership_Fee();
     }
 
+    //회비 잔액 검사
+    public Boolean BalanceCheck(int price) throws Exception {
+        Integer price_sum = depositRepository.findSUMprice();
+        if(price_sum == null) throw new Exception("입금한 데이터가 없습니다.");
+        if(price_sum < price) throw new Exception("입금 금액 보다 사용 금액이 더 많습니다.");
+        return true;
+    }
 }
